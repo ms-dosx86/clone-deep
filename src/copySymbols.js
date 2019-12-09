893e159cf1614ecc6bbbed6a93fd28b100191c22
Bayyr Oorjak <bayyr.oorjak@saritasa.com>
2019-12-09 16:26:12 +0700

    init

---
+++ src/copySymbols.js
@@ -0,0 +1,16 @@
+const copyObject = require('./copyObject.js');
+const getSymbols = require('./getSymbols.js');
+
+/**
+ * Copies own symbols of `source` to `object`.
+ *
+ * @private
+ * @param {Object} source The object to copy symbols from.
+ * @param {Object} [object={}] The object to copy symbols to.
+ * @returns {Object} Returns `object`.
+ */
+function copySymbols(source, object) {
+  return copyObject(source, getSymbols(source), object)
+}
+
+module.exports = copySymbols
